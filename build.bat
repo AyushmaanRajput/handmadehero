c2dc83db4551a50cb0079403384e4ce75a0ae680
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2025-03-07 15:44:51 +0530

    1: Added First Window ( Epilepsy warning )

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+mkdir C:\Users\hp\Desktop\handmadehero\build
+pushd C:\Users\hp\Desktop\handmadehero\build
+cl -Zi C:\Users\hp\Desktop\handmadehero\code\handmadehero.cpp /link user32.lib gdi32.lib
+popd
+
